7681a2aca38db96dc01a86a8ab27147bc0642134
cvijetin-mladenovic <cvijetin.mladenovic@hpe.com>
2023-02-11 20:37:17 +0100

    Update all

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:usluge_client/constant.dart';
+import 'package:usluge_client/screen/login/login_screen.dart';
+import 'package:usluge_client/screen/main/main_screen.dart';
 import 'package:usluge_client/screen/splash/splash_screen.dart';
 
+// An implementation of the HttpClient interface
+class MyHttpClient implements HttpClient {
+  MyHttpClient(SecurityContext? c);
+
+  @override
+  noSuchMethod(Invocation invocation) {
+    // your implementation here
+  }
+}
+
 void main() {
+  HttpOverrides.runZoned(() {
+    // Operations will use MyHttpClient instead of the real HttpClient
+    // implementation whenever HttpClient is used.
+  }, createHttpClient: (SecurityContext? c) => MyHttpClient(c));
+
   runApp(const MyApp());
 }
 
@@ -19,7 +38,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: mBackgroundColor,
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: const LoginScreen(),
     );
   }
 }
